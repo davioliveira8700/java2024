8fce5b6565b151f5f07787d0f0c4bdc0120362d5
Davi de Oliveira <odavi278@gmail.com>
2024-01-30 15:26:34 -0300

    Exercicio 3

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,21 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-
+        
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-	    int cod = sc.nextInt();
-	    int quantidade = sc.nextInt();
-		double total;
-		if(cod == 1) {
-			total = quantidade * 4.00;
-		}else if(cod == 2) {
-			total = quantidade * 4.50;
-		}else if(cod == 3) {
-			total = quantidade * 5.00;
+	    double num = sc.nextDouble();
+	    if(num >=0 && num < 25) {
+	    	System.out.println("INTERVALO (0,25)");
+	    }else if(num > 25 && num <=50) {
+	    	System.out.println("INTERVALO (25,50)");
+	    }else if(num > 50 && num <=75) {
+	    	System.out.println("INTERVALO (50,75)");
+	    }else if(num > 75 && num <=100) {
+	    	System.out.println("INTEVALO (75,100)");
+	    }else {
+	    	System.out.println("FORA DO INTEVALO");
 	    }
-		 else if(cod == 4) {
-				total = quantidade * 2.00;
-		}else {
-				total = quantidade * 1.50;
-		}
-		System.out.printf("TOTAL %.2f", total);
-		
 	
 		
 		sc.close();
