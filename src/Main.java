02da780a65b32ca7126573749ad31030aea8f48c
Davi de Oliveira <odavi278@gmail.com>
2024-01-30 11:53:59 -0300

    Saida de dados

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Olá mundo");
+		 
+		String product1 = "Computer";
+		String product2 = "Ofice-desk";
+		
+	    int age = 30;
+	    int code = 3290;
+	    char gender = 'f';
+	    
+	    double price1 = 2100.0;
+	    double price2 = 650.50;
+	    double measure = 53.234567;
+	    Locale.setDefault(Locale.US);
+	    System.out.println("PRODUCTS: ");
+	    System.out.printf("%s, wich price is $ %.2f%n", product1, price1);
+	    System.out.printf("%s, wich price is $ %.2f%n", product2, price2);
+	    
+	    System.out.printf("Record: %d years old code: %d and gender: %s", age, code, gender);
+		
 
 	}
 
