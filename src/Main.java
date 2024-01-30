b70aac67354ce5c175c71a8e2400b58ca88ce40e
Davi de Oliveira <odavi278@gmail.com>
2024-01-30 14:50:10 -0300

    Exercicio 2

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,18 @@ public class Main {
 	public static void main(String[] args) {
 		 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Digite um numero qualquer? ");
-		int num = sc.nextInt();
-		if(num <0) {
-			System.out.println("NEGATIVO");
-		}else {
-			System.out.println("NÃO NEGATIVO");
-		}
+		System.out.println("Digite o numero1? ");
+		int num1 = sc.nextInt();
+		System.out.println("Digite o numero2? ");
+		int num2 = sc.nextInt();
 		
+		if(num1 % num2 == 0) {
+			System.out.println("SÃO MULTIPLOS");
+		}else if(num2 % num1 == 0) {
+			System.out.println("SÃO MULTIPLOS");
+		}else {
+			System.out.println("NÃO SÃO MULTIPLOS");
+		}
 		sc.close();
 	}
 
