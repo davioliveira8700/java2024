59bef4db0ceb5ed2e263b0a2f4fedf3c2156f584
Davi de Oliveira <odavi278@gmail.com>
2024-01-30 14:38:29 -0300

    Estruturas condicionais

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,20 @@
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		 
-		String product1 = "Computer";
-		String product2 = "Ofice-desk";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite um numero qualquer? ");
+		int num = sc.nextInt();
+		if(num <0) {
+			System.out.println("NEGATIVO");
+		}else {
+			System.out.println("NÃO NEGATIVO");
+		}
 		
-	    int age = 30;
-	    int code = 3290;
-	    char gender = 'f';
-	    
-	    double price1 = 2100.0;
-	    double price2 = 650.50;
-	    double measure = 53.234567;
-	    Locale.setDefault(Locale.US);
-	    System.out.println("PRODUCTS: ");
-	    System.out.printf("%s, wich price is $ %.2f%n", product1, price1);
-	    System.out.printf("%s, wich price is $ %.2f%n", product2, price2);
-	    
-	    System.out.printf("Record: %d years old code: %d and gender: %s", age, code, gender);
-		
-
+		sc.close();
 	}
 
 }
