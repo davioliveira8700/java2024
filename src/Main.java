c9698c7b91e3f5e857568ef3efbb08a13751f047
Davi de Oliveira <odavi278@gmail.com>
2024-01-31 10:57:53 -0300

    Atribuicao comulativa

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,13 @@ public class Main {
         
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-	    double num = sc.nextDouble();
-	    if(num >=0 && num < 25) {
-	    	System.out.println("INTERVALO (0,25)");
-	    }else if(num > 25 && num <=50) {
-	    	System.out.println("INTERVALO (25,50)");
-	    }else if(num > 50 && num <=75) {
-	    	System.out.println("INTERVALO (50,75)");
-	    }else if(num > 75 && num <=100) {
-	    	System.out.println("INTEVALO (75,100)");
-	    }else {
-	    	System.out.println("FORA DO INTEVALO");
-	    }
-	
-		
-		sc.close();
+
+		int min = sc.nextInt();
+		double valor = 50.0;
+		if(min > 100) {
+			valor = valor + (min - 100) *2.0;
+		}
+		System.out.printf("Valor da conta %.2f R$", valor);
 	}
 
 }
