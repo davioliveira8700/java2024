5fad119bda1a9299bf530bb040a61c0cdbab9f61
Davi de Oliveira <odavi278@gmail.com>
2024-02-01 14:36:18 -0300

    Exercicio 2

--- src/EstruturaRepeticao.java
+++ src/EstruturaRepeticao.java
@@ -8,12 +8,25 @@ public class EstruturaRepeticao {
 		Scanner sc = new Scanner(System.in);
 
 	int x = sc.nextInt();
-	int impares = 0;
-	for(int i = 0; i <= x; i++) {
-		if(i % 2 !=0) {
-			System.out.println(i);
+	int in =0;
+	int out =0;
+	int y = 0;
+
+	for(int i = 0; i < x; i++) {
+		y = sc.nextInt();
+		
+		if(y >= 10 && y <=20) {
+			 in += + 1;
+			
+		}else if(y < 10 || y >20) {
+			 out += +1;
+			
 		}
+	
 	}
+	
+	System.out.println("in = " + in);
+	System.out.println("out = " + out);
 	
 		
 		sc.close();
